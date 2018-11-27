acca8da564e9f351eca5d4bf621b2b207ebdf953
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2018-11-27 23:34:03 +0200

    Initial Commit

---
+++ nsga3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nsga3
+//
+//  Created by Viktoras Laukevicius on 27/11/2018.
+//  Copyright © 2018 Viktoras Laukevicius. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
